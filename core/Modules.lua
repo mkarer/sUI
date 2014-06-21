6add03c5a6aa9f352fc20df17d5aa37d94c1dd79
Martin Karer <sezz@sezz.at>
2014-06-21 22:37:14 +0200

    added path detection based on the caller when loading the main xml

--- core/Modules.lua
+++ core/Modules.lua
@@ -15,6 +15,14 @@ local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
 
 local modulePrototype = {
 	InitializeForms = function(self, strModulePath)
+		if (not strModulePath) then
+			local debugInfo = debug.getinfo(2);
+			local caller = string.gsub(debugInfo.short_src, "\\", "/");
+			local pathRootIndex = caller:find("Addons/sUI/") + 11;
+			local dir, file, ext = string.match(caller:sub(pathRootIndex), "(.-)([^/]-([^%.]+))$");
+			strModulePath = dir:sub(1, -2);
+		end
+
 		self.xmlDoc = XmlDoc.CreateFromFile(strModulePath.."/"..self:GetName()..".xml");
 	end,
 };
