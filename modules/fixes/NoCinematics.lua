0a8b6f1ddaeee4864700ddce92464f6d529f2623
Martin Karer <sezz@sezz.at>
2014-06-22 21:26:24 +0200

    reenabled NoCinematics, still not working, but i'll just let in enabled

--- modules/fixes/NoCinematics.lua
+++ modules/fixes/NoCinematics.lua
@@ -21,47 +21,24 @@ end
 
 function M:OnEnable()
 	log:debug("%s enabled.", self:GetName());
-	self:DisableCinematics();
 
-	Apollo.RegisterEventHandler("CinematicsNotify", "OnCinematicsNotify", self);
-	Apollo.RegisterEventHandler("CinematicsCancel", "OnCinematicsCancel", self);
+	-- Unit.CodeEnumCCState.DisableCinematic
+
+--	Apollo.RegisterEventHandler("CinematicsNotify", function() log:debug("CinematicsNotify"); end);
+--	Apollo.RegisterEventHandler("CinematicsCancel", function() log:debug("CinematicsCancel"); end);
+	Apollo.RegisterEventHandler("ApplyCCState", "OnApplyCCState", self);
+--	Apollo.RegisterEventHandler("RemoveCCState", function() log:debug("RemoveCCState"); end);
+--	Apollo.RegisterEventHandler("ChangeWorld", function() log:debug("ChangeWorld"); end);
+--	Apollo.RegisterEventHandler("UnitCreated", function() log:debug("UnitCreated"); end);
 end
 
 function M:OnDisable()
 	log:debug("%s disabled.", self:GetName());
-
-	local tCinematics = Apollo.GetAddon("Cinematics");
-	Apollo.RegisterEventHandler("CinematicsNotify", "OnCinematicsNotify", tCinematics);
-	Apollo.RegisterEventHandler("CinematicsCancel", "OnCinematicsCancel", tCinematics);
 end
 
-function M:DisableCinematics()
-	if (not Apollo.GetAddonInfo("Cinematics") or not Apollo.GetAddonInfo("Cinematics").bLoadOnStart) then
-		-- Cinemeatics Addon is disabled :)
-		return;
+function M:OnApplyCCState(code, userdata)
+	if (unitPlayer == userdata and code == Unit.CodeEnumCCState.DisableCinematic) then
+		log:debug("Skipping Cinematic");
+		GameLib.UIExitCinematics();
 	end
-
-	local tCinematics = Apollo.GetAddon("Cinematics");
-	self:Unhook(tCinematics, "OnLoad");
-
-	if (tCinematics.wndCin) then
-		log:debug("Removing Events");
-		Apollo.RemoveEventHandler("CinematicsNotify", tCinematics);
-		Apollo.RemoveEventHandler("CinematicsCancel", tCinematics);
-	else
-		self:PostHook(tCinematics, "OnLoad", "DisableCinematics");
-	end
-end
-
-function M:OnCinematicsNotify(msg, param)
-	log:debug("OnCinematicsNotify");
-	log:debug(msg);
-	log:debug(param);
-	self.cinematic = param;
-	Cinematics_Cancel(param);
-end
-
-function M:OnCinematicsCancel(param)
-	log:debug("OnCinematicsCancel");
-	log:debug(param);
 end
