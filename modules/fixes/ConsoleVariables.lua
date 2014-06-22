1984c5736b365b1ae83b1e63e5b3fbece1ef6341
Martin Karer <sezz@sezz.at>
2014-06-23 01:55:28 +0200

    more useful console variables

--- modules/fixes/ConsoleVariables.lua
+++ modules/fixes/ConsoleVariables.lua
@@ -27,10 +27,13 @@ function M:OnEnable()
 	log:debug("%s enabled.", self:GetName());
 
 	-- Update Console Variables
-	Apollo.SetConsoleVariable("camera.FovY", 65); -- http://www.rjdown.co.uk/projects/bfbc2/fovcalculator.php
+	Apollo.SetConsoleVariable("camera.FovY", 60);			-- http://www.rjdown.co.uk/projects/bfbc2/fovcalculator.php
+	Apollo.SetConsoleVariable("chat.filter", false);		-- Disable Profanity Filter
+	Apollo.SetConsoleVariable("hud.healthTextDisplay", 2);	-- Show Health Text
+	Apollo.SetConsoleVariable("hud.timeDisplay", 1); 		-- Show Clock
+	Apollo.SetConsoleVariable("ui.TooltipDelay", 0);		-- Disable Tooltip Delay
 end
 
 function M:OnDisable()
 	log:debug("%s disabled.", self:GetName());
 end
-
