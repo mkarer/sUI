9749d5c68e23f7601161dc752c285eb3d55ce9c5
Martin Karer <sezz@sezz.at>
2014-06-24 16:51:20 +0200

    xp bar styling

--- modules/UnitFrames/Experience.lua
+++ modules/UnitFrames/Experience.lua
@@ -41,7 +41,60 @@ function M:MoveExperienceBars()
 	if (tXPBar.wndMain ) then
 		log:debug("Skinning Experience Bars", self:GetName());
 
-		tXPBar.wndMain:SetAnchorOffsets(156, -239, -472, -187);
+		local colorExperience = ApolloColor.new("xkcdDarkSkyBlue"); -- xkcdClearBlue xkcdDarkSkyBlue
+		local colorRested = ApolloColor.new("xkcdDeepLavender"); -- xkcdDarkPeriwinkle xkcdAmethyst
+
+		-- Move Main Frame
+		tXPBar.wndMain:SetAnchorPoints(0.25, 1, 0.25, 1);
+		tXPBar.wndMain:SetAnchorOffsets(0, -200, 300, -120);
+
+		-- Move XP Button
+		tXPBar.wndMain:FindChild("XPButton"):SetAnchorPoints(0, 0, 0, 0);
+		tXPBar.wndMain:FindChild("XPButton"):SetAnchorOffsets(0, 0, 73, 23);
+
+		-- Move XP Bar
+		tXPBar.wndMain:FindChild("XPBarContainer"):SetAnchorPoints(0, 0, 1, 0);
+		tXPBar.wndMain:FindChild("XPBarContainer"):SetAnchorOffsets(73, 5, 0, 13);
+		tXPBar.wndMain:FindChild("XPBarContainer"):SetSprite("SezzUIBorderDark");
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("RestXPBarFill"):SetAnchorPoints(0, 0, 1, 1);
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("RestXPBarFill"):SetAnchorOffsets(2, 2, -2, -2);
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("RestXPBarGoal"):SetAnchorPoints(0, 0, 1, 1);
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("RestXPBarGoal"):SetAnchorOffsets(2, 2, -2, -2);
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("RestXPBarGoal"):SetBarColor(colorRested);
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("RestXPBarGoal"):SetFullSprite("ProgressBar");
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("XPBarFill"):SetAnchorPoints(0, 0, 1, 1);
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("XPBarFill"):SetAnchorOffsets(2, 2, -2, -2);
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("XPBarFill"):SetBarColor(colorExperience);
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("XPBarFill"):SetFullSprite("ProgressBar");
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("DailyMaxEPBar"):SetAnchorPoints(0, 0, 1, 1);
+		tXPBar.wndMain:FindChild("XPBarContainer"):FindChild("DailyMaxEPBar"):SetAnchorOffsets(2, 2, -2, -2);
+
+		-- Move Path XP Button
+		tXPBar.wndMain:FindChild("PathButton"):SetAnchorPoints(0, 0, 0, 0);
+		tXPBar.wndMain:FindChild("PathButton"):SetAnchorOffsets(0, 23, 73, 46);
+
+		-- Move Path XP Bar
+		tXPBar.wndMain:FindChild("PathBarContainer"):SetAnchorPoints(0, 0, 1, 0);
+		tXPBar.wndMain:FindChild("PathBarContainer"):SetAnchorOffsets(73, 28, 0, 49);
+		tXPBar.wndMain:FindChild("PathBarContainer"):SetSprite("SezzUIBorderDark");
+		tXPBar.wndMain:FindChild("PathBarContainer"):FindChild("PathBarFill"):SetAnchorPoints(0, 0, 1, 1);
+		tXPBar.wndMain:FindChild("PathBarContainer"):FindChild("PathBarFill"):SetAnchorOffsets(2, 2, -2, -2);
+		tXPBar.wndMain:FindChild("PathBarContainer"):FindChild("PathBarFill"):SetFullSprite("ProgressBar");
+		tXPBar.wndMain:FindChild("PathBarContainer"):FindChild("PathBarFill"):SetBarColor(colorExperience);
+
+		-- Move Path Icon
+		tXPBar.wndMain:FindChild("PathIcon"):SetAnchorPoints(0, 0, 0, 0);
+		tXPBar.wndMain:FindChild("PathIcon"):SetAnchorOffsets(8, 30, 26, 48);
+
+		-- Clickable Bars
+		-- Using MouseButtonDown until they add a Click/Signal event...
+		tXPBar.wndMain:FindChild("XPBarContainer"):RemoveStyle("IgnoreMouse");
+		tXPBar.wndMain:FindChild("XPBarContainer"):AddEventHandler("MouseButtonDown", "OnXpClicked", tXPBar);
+		tXPBar.wndMain:FindChild("PathBarContainer"):RemoveStyle("IgnoreMouse");
+		tXPBar.wndMain:FindChild("PathBarContainer"):AddEventHandler("MouseButtonDown", "OnPathClicked", tXPBar);
+
+
+		S:ApplyDebugBackdrop(tXPBar.wndMain);
 	else
 		self:PostHook(tXPBar, "OnDocumentReady", "MoveExperienceBars");
 	end
