2f409d42288bafa92398ba59a3293632172cb5a5
Martin Karer <sezz@sezz.at>
2014-06-25 03:45:04 +0200

    added short emote slash commands and reloadui

--- core/slash.lua
+++ core/slash.lua
@@ -1,12 +1,37 @@
-local S = Apollo.GetAddon("s:UI")
-local module = {}
+--[[
 
--- Module Loader
-function module:OnEnable()
-	Apollo.RegisterSlashCommand("rl", "ReloadUI", self)
-end
+	s:UI Slash Commands
+
+	Martin Karer / Sezz, 2014
+	http://www.sezz.at
+
+--]]
+
+require "ChatSystemLib";
+
+-----------------------------------------------------------------------------
+
+local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
+local M = {};
+
+-----------------------------------------------------------------------------
 
 -- Reload UI
-function module:ReloadUI()
-	RequestReloadUI();
+Apollo.RegisterSlashCommand("rl", "RequestReloadUI");
+
+-- Emotes
+local emoteShortcuts = {
+	ty = "thanks",
+	thx = "thanks",
+	lol = "laugh",
+};
+
+function M:DoEmote(emote, target)
+	if (emote and emoteShortcuts[emote]) then
+		ChatSystemLib.Command("/"..emoteShortcuts[emote].. " "..target);
+	end
+end
+
+for emote, v in pairs(emoteShortcuts) do
+	Apollo.RegisterSlashCommand(emote, "DoEmote", M);
 end
