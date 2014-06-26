fc3b4957ed78946211bbca086ad12b7594ed2581
Martin Karer <sezz@sezz.at>
2014-06-26 18:20:57 +0200

    removed loop, updated to use DestroyAllPixies

--- core/Media.lua
+++ core/Media.lua
@@ -12,19 +12,7 @@ local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
 -----------------------------------------------------------------------------
 
 function S:RemoveArtwork(f)
-	-- Destroy Pixies
-	local i = 1;
-	while (true) do
-		local pixie = f:GetPixieInfo(i);
-		if (pixie) then
-			f:DestroyPixie(i);
-			i = i + 1;
-		else
-			break;
-		end
-	end
-
-	-- Unset Sprite
+	f:DestroyAllPixies();
 	f:SetSprite(nil);
 end
 
