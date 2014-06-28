42446d40fb88f4d0c300ca1efd7ba0f6ca9bfb40
Martin Karer <sezz@sezz.at>
2014-06-28 23:13:16 +0200

    added datachron events, should be fully working

--- Modules/MiniMap/Buttons.lua
+++ Modules/MiniMap/Buttons.lua
@@ -9,7 +9,7 @@
 
 local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
 local MiniMap = S:GetModule("MiniMap");
-local M = MiniMap:CreateSubmodule("Buttons");
+local M = MiniMap:CreateSubmodule("Buttons", "Gemini:Event-1.0");
 local log;
 
 -----------------------------------------------------------------------------
@@ -20,18 +20,18 @@ local CreateButtonContainer;
 do
 	local CreateButton = function(self, strName, strIcon, bLastButton)
 		local tButton = {};
+		tButton.strName = strName;
 		tButton.wndMain = Apollo.LoadForm(self.xmlDoc, "SezzMiniMapButton", self.wndMain, tButton);
 		tButton.wndMain:SetName(string.format("SezzMiniMapButton%s", strName));
 		tButton.wndMain:FindChild("Icon"):SetSprite(strIcon);
 
 		-- Move Button
-		local nSize = tButton.wndMain:GetWidth();
 		if (bLastButton) then
 			tButton.wndMain:SetAnchorPoints(1, 0, 1, 1);
-			tButton.wndMain:SetAnchorOffsets(-nSize, 0, 0, 0);
+			tButton.wndMain:SetAnchorOffsets(-self.nButtonSize, 0, 0, 0);
 		else
-			local nPositionX = (#self.tButtons + 1) * (nSize + self.nButtonPadding);
-			tButton.wndMain:SetAnchorOffsets(nPositionX, 0, nPositionX + nSize, 0);
+			local nPositionX = #self.tButtons * (self.nButtonSize + self.nButtonPadding);
+			tButton.wndMain:SetAnchorOffsets(nPositionX, 0, nPositionX + self.nButtonSize, 0);
 		end
 
 		-- Done
@@ -39,13 +39,39 @@ do
 		return tButton;
 	end
 
+	local GetButton = function(self, strName)
+		for _, tButton in pairs(self.tButtons) do
+			if (tButton.strName == strName) then
+				return tButton;
+			end
+		end
+	end
+
 	CreateButtonContainer = function(xmlDoc)
 		local tContainer = {};
 		tContainer.wndMain = Apollo.LoadForm(xmlDoc, "SezzMiniMapButtonContainer", nil, tButtonContainer);
 		tContainer.tButtons = {};
 		tContainer.nButtonPadding = 7;
-		tContainer.CreateButton = CreateButton;
 		tContainer.xmlDoc = xmlDoc;
+
+		tContainer.CreateButton = CreateButton;
+		tContainer.GetButton = GetButton;
+
+		-- Find Button Size
+		local nButtonSize = 30;
+		for _, f in pairs(xmlDoc:ToTable()) do
+			if (f.Name and f.Name == "SezzMiniMapButton") then
+				nButtonSize = tonumber(f.RAnchorOffset) - tonumber(f.LAnchorOffset);
+				break;
+			end
+		end
+
+		tContainer.nButtonSize = nButtonSize;
+
+		-- Move Container
+		local nOffsetL, nOffsetT, nOffsetR, nOffsetB = tContainer.wndMain:GetAnchorOffsets();
+		nOffsetL = nOffsetL + tContainer.nButtonPadding + tContainer.nButtonSize;
+		tContainer.wndMain:SetAnchorOffsets(nOffsetL, nOffsetT, nOffsetR, nOffsetB);
 
 		return tContainer;
 	end
@@ -64,26 +90,68 @@ function M:OnEnable()
 	log:debug("%s enabled.", self:GetName());
 
 	-- Add Buttons
-	local tButtonContainer = CreateButtonContainer(self.xmlDoc);
+	self.tButtonContainer = CreateButtonContainer(self.xmlDoc);
 
+	-----------------------------------------------------------------------------
 	-- Inventory
-	tButtonContainer:CreateButton("Inventory", "IconInventory");
+	-----------------------------------------------------------------------------
 
+	self.tButtonContainer:CreateButton("Inventory", "IconInventory");
+
+	-----------------------------------------------------------------------------
 	-- Datachron
-	local tButtonDatachron = tButtonContainer:CreateButton("Datachron", "IconDatachron");
---	local tDatachron = Apollo.GetAddon("Datachron");
---	tButtonDatachron:AddEventHandler("ButtonCheck", "OnRestoreDatachron", tDatachron);
---	tButtonDatachron:AddEventHandler("ButtonUncheck", "OnMinimizeDatachron", tDatachron);
+	-----------------------------------------------------------------------------
+	self.tButtonContainer:CreateButton("Datachron", "IconDatachron");
+	if (g_wndDatachron) then
+		self:UpdateDatachronButton();
+	else
+		self:RegisterEvent("Sezz_AddonAvailable", "UpdateDatachronButton");
+	end
 
 	-- Dash Indicator
-	tButtonContainer:CreateButton("Dash", "IconDash2");
+	self.tButtonContainer:CreateButton("Dash", "IconDash2");
 
 	-- Settings
-	tButtonContainer:CreateButton("Settings", "IconSettings", true);
+	self.tButtonContainer:CreateButton("Settings", "IconSettings", true);
 
-	-- Remove XML Document
+	-- Done
 	self.xmlDoc = nil;
 end
 
-function M:CreateButton(strName, strIcon)
+function M:UpdateDatachronButton(strEvent, strAddon)
+	if (not strEvent or strAddon == "Datachron") then
+		local tButtonDatachron = self.tButtonContainer:GetButton("Datachron");
+
+		tButtonDatachron.UpdateTooltip = function(self)
+			if (self.wndMain:IsChecked()) then
+				self.wndMain:SetTooltip(Apollo.GetString("CRB_Datachron_MinimizeBtn_Desc"));
+			else
+				self.wndMain:SetTooltip(Apollo.GetString("Datachron_Maximize"));
+			end
+		end
+
+		tButtonDatachron.ToggleDatachron = function(self, wndHandler)
+			g_wndDatachron:Show(wndHandler:IsChecked());
+			self:UpdateTooltip();
+		end
+
+		tButtonDatachron.wndMain:AddEventHandler("ButtonCheck", "ToggleDatachron", tButtonDatachron);
+		tButtonDatachron.wndMain:AddEventHandler("ButtonUncheck", "ToggleDatachron", tButtonDatachron);
+		tButtonDatachron.wndMain:SetCheck(g_wndDatachron:IsVisible());
+		tButtonDatachron:UpdateTooltip();
+
+		Apollo.GetAddon("Datachron").wndMinimized:Show(false, false);
+
+		g_wndDatachron:SetAnchorOffsets(-549, -322, -160, -18);
+
+		-- Removing Artwork only works while logging in.
+		-- I don't care currently...
+--		g_wndDatachron:DestroyAllPixies();
+--		g_wndDatachron:FindChild("Framing"):SetSprite("SezzUIBorder");
+--		g_wndDatachron:FindChild("Framing"):SetSprite(nil);
+
+		if (strEvent) then
+			self:UnregisterEvent(strEvent);
+		end
+	end
 end
