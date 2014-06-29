4e479329b00f4d6f6efe9a31755e0026a2458d40
Martin Karer <sezz@sezz.at>
2014-06-29 06:06:43 +0200

    updated the template to use CreateSubmodule

--- Modules/Template.lua
+++ Modules/Template.lua
@@ -8,7 +8,7 @@
 --]]
 
 local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
-local M = S:NewModule("ActionBars", "Gemini:Event-1.0", "Gemini:Hook-1.0");
+local M = S:CreateSubmodule("ActionBars", "Gemini:Hook-1.0");
 local log;
 
 -----------------------------------------------------------------------------
