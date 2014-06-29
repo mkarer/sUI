3a3c4bb1a42e60db8918a54becb059a1ba271b89
Martin Karer <sezz@sezz.at>
2014-06-29 07:14:24 +0200

    removed all the complicated stuff, a simple SetTooltipType(0) does magic

--- Modules/Tooltips/Tooltips.lua
+++ Modules/Tooltips/Tooltips.lua
@@ -8,7 +8,7 @@
 --]]
 
 local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
-local M = S:CreateSubmodule("Tooltips");
+local M = S:CreateSubmodule("Tooltips", "Gemini:Hook-1.0");
 local log;
 
 -----------------------------------------------------------------------------
@@ -17,47 +17,10 @@ local log;
 
 function M:OnInitialize()
 	log = S.Log;
-	self:InitializeForms();
 
 	local tTooltips = Apollo.GetAddon("ToolTips");
 	if (tTooltips) then
-		-- OnLoad Hook
-		-- Applies our XML
-		tTooltips.OnLoad = function(self)
-			self.xmlDoc = M.xmlDoc;
-			self.xmlDoc:RegisterCallback("OnDocumentReady", self);
-		end
-
-		-- OnDocumentReady Hook
-		-- Applies our WorldTooltipContainer (they hardcoded TooltipsForms.xml)
-		tTooltips._OnDocumentReady = tTooltips.OnDocumentReady;
-		tTooltips.OnDocumentReady = function(self)
-			-- Disable GameLib.SetWorldTooltipContainer
-			GameLib._SetWorldTooltipContainer = GameLib.SetWorldTooltipContainer;
-			GameLib.SetWorldTooltipContainer = S.Dummy;
-			-- Call Original OnDocumentReady
-			self:_OnDocumentReady();
-			-- Create WorldTooltipContainer 
-			self.wndContainer = Apollo.LoadForm(self.xmlDoc, "WorldTooltipContainer", nil, self);
-			-- Enable GameLib.SetWorldTooltipContainer
-			GameLib.SetWorldTooltipContainer = GameLib._SetWorldTooltipContainer;
-			GameLib._SetWorldTooltipContainer = nil;
-			-- Apply WorldTooltipContainer 
-			GameLib.SetWorldTooltipContainer(self.wndContainer);
-		end
-
---[[
-		-- Apollo.LoadForm Hook
-		-- Why is everything in ToolTips.lua hardcoded?!
-		Apollo._LoadForm = Apollo.LoadForm;
-		Apollo.LoadForm = function(strFile, ...)
-			if (type(strFile) == "string" and (strFile == "TooltipsForms.xml" or strFile == "ui\\Tooltips\\TooltipsForms.xml")) then
-				strFile = tTooltips.xmlDoc;
-			end
-
-			return Apollo._LoadForm(strFile, ...);
-		end
-]]
+		self:PostHook(tTooltips, "OnDocumentReady", "UpdateWorldTooltipContainer");
 	else
 		self:SetEnabledState(false);
 	end
@@ -69,4 +32,8 @@ end
 
 function M:OnDisable()
 	log:debug("%s disabled.", self:GetName());
+end
+
+function M:UpdateWorldTooltipContainer()
+	GameLib.GetWorldTooltipContainer():SetTooltipType(0);
 end
