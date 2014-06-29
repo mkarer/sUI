75b19ebb59c1642634d0444d5392ed4e91aeec1c
Martin Karer <sezz@sezz.at>
2014-06-29 07:15:01 +0200

    saved the xml manipulation stuff here, I'll propably use it for the minimap later

--- Core/Temp.lua
+++ Core/Temp.lua
@@ -39,4 +39,51 @@ function M:EventHandler(event, ...)
 			log:debug(spl.tTiers[1].splObject:GetIcon());
 		end
 	end
+
+		-- OnLoad Hook
+		tTooltips._OnLoad = tTooltips.OnLoad;
+		tTooltips.OnLoad = function(self)
+			self:_OnLoad();
+
+			local tXml = self.xmlDoc:ToTable();
+			for _, tNode in pairs(tXml) do
+				if (tNode.Name and tNode.Name == "WorldTooltipContainer") then
+					tNode.TooltipType = "OnCursor";
+					break;
+				end
+			end
+
+			self.xmlDoc = XmlDoc.CreateFromTable(tXml);
+		end
+
+		-- OnDocumentReady Hook
+		-- Applies our WorldTooltipContainer (Carbine uses TooltipsForms.xml EVERYWHERE instead of self.xmlDoc)
+		tTooltips._OnDocumentReady = tTooltips.OnDocumentReady;
+		tTooltips.OnDocumentReady = function(self)
+			-- Disable GameLib.SetWorldTooltipContainer
+			GameLib._SetWorldTooltipContainer = GameLib.SetWorldTooltipContainer;
+			GameLib.SetWorldTooltipContainer = S.Dummy;
+			-- Call Original OnDocumentReady
+			self:_OnDocumentReady();
+			-- Create WorldTooltipContainer 
+			self.wndContainer = Apollo.LoadForm(self.xmlDoc, "WorldTooltipContainer", nil, self);
+			-- Enable GameLib.SetWorldTooltipContainer
+			GameLib.SetWorldTooltipContainer = GameLib._SetWorldTooltipContainer;
+			GameLib._SetWorldTooltipContainer = nil;
+			-- Apply WorldTooltipContainer 
+			GameLib.SetWorldTooltipContainer(self.wndContainer);
+		end
+
+--[[
+		-- Apollo.LoadForm Hook
+		-- Why is everything in ToolTips.lua hardcoded?!
+		Apollo._LoadForm = Apollo.LoadForm;
+		Apollo.LoadForm = function(strFile, ...)
+			if (type(strFile) == "string" and (strFile == "TooltipsForms.xml" or strFile == "ui\\Tooltips\\TooltipsForms.xml")) then
+				strFile = tTooltips.xmlDoc;
+			end
+
+			return Apollo._LoadForm(strFile, ...);
+		end
+]]
 end
