a2cd6aa9eac3d0cb2656c37b1e9d75dce09a32ef
Martin Karer <sezz@sezz.at>
2014-06-29 06:06:33 +0200

    simple utilities for debugging

---
+++ Core/Debug.lua
@@ -0,0 +1,29 @@
+--[[
+
+	s:UI Debugging Stuff
+
+	Not needed for any module or the core itself.
+
+	Martin Karer / Sezz, 2014
+	http://www.sezz.at
+
+--]]
+
+local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
+
+-----------------------------------------------------------------------------
+
+function S:DumpAbility(strName, bCompleteDump)
+	local tAbilities = AbilityBook.GetAbilitiesList();
+
+	for _, tAbility in pairs(tAbilities) do
+		if (tAbility.strName == strName) then
+			S.Log:debug("ID: %d", tAbility.nId);
+			S.Log:debug("Icon: %s", tAbility.tTiers[1].splObject:GetIcon());
+
+			if (bCompleteDump) then
+				S.Log:debug(tAbility);
+			end
+		end
+	end
+end
