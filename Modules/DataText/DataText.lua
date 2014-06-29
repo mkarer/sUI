ee2a8ba23622f94637c080af32584598c8a6cc86
Martin Karer <sezz@sezz.at>
2014-06-29 13:36:04 +0200

    added clock, fps and latency

--- Modules/DataText/DataText.lua
+++ Modules/DataText/DataText.lua
@@ -8,8 +8,9 @@
 --]]
 
 local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
-local M = S:CreateSubmodule("DataText", "Gemini:Hook-1.0");
+local M = S:CreateSubmodule("DataText");
 local log;
+local strformat, tostring = string.format, tostring;
 
 -----------------------------------------------------------------------------
 -- Initialization
@@ -22,6 +23,22 @@ end
 
 function M:OnEnable()
 	log:debug("%s enabled.", self:GetName());
+
+	self.tFontSmall = S:CreatePixelFont("04b_11", 8);
+	self.tFontLarge = S:CreatePixelFont("04b_11", 24);
+	self.tColorClock = CColor.new(1, 1, 1, 0.2);
+
+	self.wndMain = Apollo.LoadForm(self.xmlDoc, "DataText", nil, self);
+	self.wndMain:Show(true, true);
+
+	-- Start Clock Timer + Update Time
+	self.timerClock = ApolloTimer.Create(2, true, "UpdateText", self);
+	self.timerMemory = ApolloTimer.Create(300, true, "UpdateAddondMemory", self); -- LAAAAG
+	self.timerStats = ApolloTimer.Create(3, true, "UpdateLatency", self);
+
+	self:UpdateAddondMemory();
+	self:UpdateLatency();
+	self:UpdateText();
 end
 
 function M:OnDisable()
@@ -29,6 +46,181 @@ function M:OnDisable()
 end
 
 -----------------------------------------------------------------------------
--- Code
+-- Clock
 -----------------------------------------------------------------------------
 
+function M:UpdateText(a)
+	-- Get Time
+	local tTime = GameLib.GetLocalTime();
+	local strTime = strformat("%02d:%02d", tostring(tTime.nHour), tostring(tTime.nMinute));
+
+	-- Update Text
+	local strStats = strformat("%dFPS %.1fMB %dMS 100%%", self.nFPS, self.nAddonMemory, self.nLatency);
+
+	self.tFontLarge:Draw(self.wndMain:FindChild("Clock"), strTime, true, self.tColorClock);
+	self.tFontSmall:Draw(self.wndMain:FindChild("Stats"), strStats, true);
+end
+
+
+-----------------------------------------------------------------------------
+-- Latency / FPS
+-----------------------------------------------------------------------------
+
+function M:UpdateLatency()
+	self.nLatency = GameLib.GetLatency();
+	self.nFPS = GameLib.GetFrameRate();
+end
+
+-----------------------------------------------------------------------------
+-- Addon Memory
+-----------------------------------------------------------------------------
+
+local tAddonsCarbine = {
+	["Abilities"] = true,
+	["AbilityAMPs"] = true,
+	["AbilityVendor"] = true,
+	["AccountInventory"] = true,
+	["Achievements"] = true,
+	["ActionBarFrame"] = true,
+	["ActionBarShortcut"] = true,
+	["AdventureClueTracker"] = true,
+	["AdventureMalgrave"] = true,
+	["AdventureNorthernWilds"] = true,
+	["AdventureWhitevale"] = true,
+	["ArenaTeam"] = true,
+	["BankViewer"] = true,
+	["BuildMap"] = true,
+	["CastBar"] = true,
+	["Challenges"] = true,
+	["Character"] = true,
+	["ChatLog"] = true,
+	["Cinematics"] = true,
+	["Circles"] = true,
+	["ClassResources"] = true,
+	["CombatLog"] = true,
+	["CommDisplay"] = true,
+	["ContextMenuPlayer"] = true,
+	["Costumes"] = true,
+	["Crafting"] = true,
+	["CraftingGrid"] = true,
+	["CraftingResume"] = true,
+	["CraftingSummaryScreen"] = true,
+	["CrowdControlGameplay"] = true,
+	["CSI"] = true,
+	["CustomerSurvey"] = true,
+	["Datachron"] = true,
+	["Death"] = true,
+	["Dialog"] = true,
+	["ErrorDialog"] = true,
+	["FloatText"] = true,
+	["FriendsList"] = true,
+	["GalacticArchive"] = true,
+	["GameExit"] = true,
+	["GroupFrame"] = true,
+	["Guild"] = true,
+	["GuildAlerts"] = true,
+	["GuildBank"] = true,
+	["GuildContentInfo"] = true,
+	["GuildContentPerks"] = true,
+	["GuildContentRoster"] = true,
+	["GuildDesigner"] = true,
+	["GuildRegistration"] = true,
+	["Hazards"] = true,
+	["HealthShieldBar"] = true,
+	["Housing"] = true,
+	["HousingAlerts"] = true,
+	["HUD"] = true,
+	["HUDAlerts"] = true,
+	["Inspect"] = true,
+	["InstanceSettings"] = true,
+	["InterfaceMenuList"] = true,
+	["Inventory"] = true,
+	["Keybinding"] = true,
+	["LevelUpUnlocks"] = true,
+	["LootNotificationWindow"] = true,
+	["LoreWindow"] = true,
+	["Macros"] = true,
+	["Mail"] = true,
+	["MarketplaceAuction"] = true,
+	["MarketplaceCommodity"] = true,
+	["MarketplaceCREDD"] = true,
+	["MarketplaceListings"] = true,
+	["MasterLoot"] = true,
+	["MatchMaker"] = true,
+	["MatchTracker"] = true,
+	["Medic"] = true,
+	["MessageManager"] = true,
+	["MiniMap"] = true,
+	["MountCustomization"] = true,
+	["Nameplates"] = true,
+	["NCCB"] = true,
+	["NeedVsGreed"] = true,
+	["NeighborList"] = true,
+	["NonCombatSpellbook"] = true,
+	["Options"] = true,
+	["OptionsInterface"] = true,
+	["PathExplorerContent"] = true,
+	["PathScientistContent"] = true,
+	["PathSettlerContent"] = true,
+	["PathSoldierContent"] = true,
+	["PlayerPath"] = true,
+	["PlayerTicket"] = true,
+	["PopupText"] = true,
+	["ProgressLog"] = true,
+	["PublicEventStats"] = true,
+	["PublicEventVote"] = true,
+	["PvPKillBoard"] = true,
+	["QuestLog"] = true,
+	["QuestTracker"] = true,
+	["RaidFrameBase"] = true,
+	["RaidFrameLeaderOptions"] = true,
+	["RaidFrameMasterLoot"] = true,
+	["RaidFrameTearOff"] = true,
+	["RecallFrame"] = true,
+	["ReportPlayer"] = true,
+	["Reputation"] = true,
+	["ResourceConversion"] = true,
+	["RewardIcons"] = true,
+	["Runecrafting"] = true,
+	["RuneSets"] = true,
+	["Sabotage"] = true,
+	["SocialPanel"] = true,
+	["SprintMeter"] = true,
+	["StalkerResource"] = true,
+	["StoryPanel"] = true,
+	["Stuck"] = true,
+	["SupplySatchel"] = true,
+	["TargetFrame"] = true,
+	["TaxiMap"] = true,
+	["TechWarrior"] = true,
+	["ToolTips"] = true,
+	["Tradeskills"] = true,
+	["TradeskillTrainer"] = true,
+	["Trading"] = true,
+	["Tutorial"] = true,
+	["Util"] = true,
+	["Vendor"] = true,
+	["Warparty"] = true,
+	["WarpartyBank"] = true,
+	["Warplots"] = true,
+	["Who"] = true,
+	["XPBar"] = true,
+	["ZoneCompletion"] = true,
+	["ZoneMap"] = true,
+};
+
+function M:UpdateAddondMemory()
+	local nAddonMemory = 0;
+	local tAddons = Apollo:GetAddons();
+
+	for _, strName in pairs(tAddons) do
+		if (not tAddonsCarbine[strName]) then
+			local tAddon = Apollo.GetAddonInfo(strName);
+			if (tAddon and tAddon.nMemoryUsage) then
+				nAddonMemory = nAddonMemory + tAddon.nMemoryUsage;
+			end
+		end
+	end
+
+	self.nAddonMemory = nAddonMemory / 1024 / 1024;
+end
