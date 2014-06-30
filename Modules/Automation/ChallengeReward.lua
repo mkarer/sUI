c7e01d8977e786e431d97830f585840b36bc4887
Martin Karer <sezz@sezz.at>
2014-06-30 11:44:06 +0200

    added module to automatically close the challenge reward spinner

---
+++ Modules/Automation/ChallengeReward.lua
@@ -0,0 +1,38 @@
+--[[
+
+	s:UI Challenge Reward Closer
+
+	Martin Karer / Sezz, 2014
+	http://www.sezz.at
+
+--]]
+
+local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
+local AutomationCore = S:GetModule("AutomationCore");
+local M = AutomationCore:CreateSubmodule("ChallengeReward");
+local log;
+
+-----------------------------------------------------------------------------
+
+function M:OnInitialize()
+	log = S.Log;
+end
+
+function M:OnEnable()
+	log:debug("%s enabled.", self:GetName());
+
+	self:RegisterEvent("ChallengeReward_SpinBegin", "CloseChallengeRewardPanel");
+end
+
+function M:OnDisable()
+	log:debug("%s disabled.", self:GetName());
+
+	self:UnregisterEvent("ChallengeReward_SpinBegin");
+end
+
+function M:CloseChallengeRewardPanel(event, ...)
+	tRewardPanel = Apollo.FindWindowByName("ChallengeRewardPanelForm");
+	if (tRewardPanel) then
+		tRewardPanel:Close();
+	end
+end
