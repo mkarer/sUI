4d17cb5a19e0ff6db6e56e0a1e5434c1c26f80dc
Martin Karer <sezz@sezz.at>
2014-07-02 06:36:29 +0200

    meh

--- Core/Temp.lua
+++ Core/Temp.lua
@@ -7,10 +7,6 @@
 
 --]]
 
-require "Window";
-
------------------------------------------------------------------------------
-
 local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
 local M = S:CreateSubmodule("Temp", "Gemini:Hook-1.0");
 local log;
@@ -46,71 +42,19 @@ function M:EventHandler(event, ...)
 		end
 	end
 
-	local nUnreadMessages = 0;
-	for idx, tMessage in pairs(MailSystemLib.GetInbox()) do
-		local tMessageInfo = tMessage:GetMessageInfo();
-		
-		if (tMessageInfo and not tMessageInfo.bIsRead) then
-			nUnreadMessages = nUnreadMessages + 1;
-		end
-	end
-
-	log:debug("[Unread] Show Mail Alert: "..(nUnreadMessages > 0 and "YES" or "NO"));
 
 --	ChallengeRewardPanel:OnWindowCloseDelay();
 
---[[	local x = AbilityBook.GetAbilitiesList();
-	for _, spl in pairs(x) do
-		if (spl.strName == "Impale") then
-			log:debug(spl.nId);
-			log:debug(spl.tTiers[1].splObject:GetIcon());
+--[[
+	-- Apollo.LoadForm Hook
+	-- Why is everything in ToolTips.lua hardcoded?!
+	Apollo._LoadForm = Apollo.LoadForm;
+	Apollo.LoadForm = function(strFile, ...)
+		if (type(strFile) == "string" and (strFile == "TooltipsForms.xml" or strFile == "ui\\Tooltips\\TooltipsForms.xml")) then
+			strFile = tTooltips.xmlDoc;
 		end
+
+		return Apollo._LoadForm(strFile, ...);
 	end
-
-		-- OnLoad Hook
-		tTooltips._OnLoad = tTooltips.OnLoad;
-		tTooltips.OnLoad = function(self)
-			self:_OnLoad();
-
-			local tXml = self.xmlDoc:ToTable();
-			for _, tNode in pairs(tXml) do
-				if (tNode.Name and tNode.Name == "WorldTooltipContainer") then
-					tNode.TooltipType = "OnCursor";
-					break;
-				end
-			end
-
-			self.xmlDoc = XmlDoc.CreateFromTable(tXml);
-		end
-
-		-- OnDocumentReady Hook
-		-- Applies our WorldTooltipContainer (Carbine uses TooltipsForms.xml EVERYWHERE instead of self.xmlDoc)
-		tTooltips._OnDocumentReady = tTooltips.OnDocumentReady;
-		tTooltips.OnDocumentReady = function(self)
-			-- Disable GameLib.SetWorldTooltipContainer
-			GameLib._SetWorldTooltipContainer = GameLib.SetWorldTooltipContainer;
-			GameLib.SetWorldTooltipContainer = S.Dummy;
-			-- Call Original OnDocumentReady
-			self:_OnDocumentReady();
-			-- Create WorldTooltipContainer 
-			self.wndContainer = Apollo.LoadForm(self.xmlDoc, "WorldTooltipContainer", nil, self);
-			-- Enable GameLib.SetWorldTooltipContainer
-			GameLib.SetWorldTooltipContainer = GameLib._SetWorldTooltipContainer;
-			GameLib._SetWorldTooltipContainer = nil;
-			-- Apply WorldTooltipContainer 
-			GameLib.SetWorldTooltipContainer(self.wndContainer);
-		end
-
-
-		-- Apollo.LoadForm Hook
-		-- Why is everything in ToolTips.lua hardcoded?!
-		Apollo._LoadForm = Apollo.LoadForm;
-		Apollo.LoadForm = function(strFile, ...)
-			if (type(strFile) == "string" and (strFile == "TooltipsForms.xml" or strFile == "ui\\Tooltips\\TooltipsForms.xml")) then
-				strFile = tTooltips.xmlDoc;
-			end
-
-			return Apollo._LoadForm(strFile, ...);
-		end
 ]]
 end
