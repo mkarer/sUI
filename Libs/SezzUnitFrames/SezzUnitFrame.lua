0d6e0a7168110d684137a2cd9d5b20d7df7077cf
Martin Karer <sezz@sezz.at>
2014-07-09 22:01:00 +0200

    removed (disabled) tooltip code, update for new aura element

--- Libs/SezzUnitFrames/SezzUnitFrame.lua
+++ Libs/SezzUnitFrames/SezzUnitFrame.lua
@@ -245,21 +245,6 @@ local OnMouseClick = function(self, wndHandler, wndControl, eMouseButton, x, y)
 end
 
 -----------------------------------------------------------------------------
--- Tooltips
------------------------------------------------------------------------------
-
--- local OnGenerateTooltip = function(self, wndControl, wndHandler, eType, arg1, arg2)
--- end
-
-local OnGenerateBuffTooltip = function(self, wndHandler, wndControl, tType, splBuff)
-	if (wndHandler == wndControl) then
-		return;
-	end
-
-	Tooltip.GetBuffTooltipForm(self, wndControl, splBuff, {bFutureSpell = false});
-end
-
------------------------------------------------------------------------------
 -- Health
 -----------------------------------------------------------------------------
 
@@ -352,10 +337,6 @@ local Disable = function(self)
 		tElement:Disable();
 	end
 
-	if (self.wndAuras) then self.wndAuras:SetUnit(nil); end
-	if (self.wndBuffs) then self.wndBuffs:SetUnit(nil); end
-	if (self.wndDebuffs) then self.wndDebuffs:SetUnit(nil); end
-
 	self.unit = nil;
 	self:Hide();
 end
@@ -365,10 +346,6 @@ local Enable = function(self)
 	for _, tElement in ipairs(self.tElements) do
 		tElement:Enable();
 	end
-
-	if (self.wndAuras) then self.wndAuras:SetUnit(self.unit); end
-	if (self.wndBuffs) then self.wndBuffs:SetUnit(self.unit); end
-	if (self.wndDebuffs) then self.wndDebuffs:SetUnit(self.unit); end
 
 	self:Update();
 	self:Show();
@@ -395,6 +372,10 @@ end
 -- Forms
 -----------------------------------------------------------------------------
 
+local FindRootOrChildWindow = function(self, strName)
+	return self.xmlDoc:LoadForm(self.strLayoutName..self.strUnit..strName, nil, self) or self.wndMain:FindChild(strName);
+end
+
 -- Load Form
 -- Adds the Unit Frame to the UI
 local LoadForm = function(self)
@@ -413,28 +394,16 @@ local LoadForm = function(self)
 	-- Enable Targetting
 	self.wndMain:AddEventHandler("MouseButtonDown", "OnMouseClick", self);
 
---	-- Enable Tooltips
---	self.wndMain:AddEventHandler("GenerateTooltip", "OnGenerateTooltip", self);
-
 	-- Add Properties for our Elements
-	self.wndCastBar = self.xmlDoc:LoadForm(self.strLayoutName..self.strUnit.."CastBar", nil, self) or self.wndMain:FindChild("CastBar");
-	self.wndExperience = self.xmlDoc:LoadForm(self.strLayoutName..self.strUnit.."Experience", nil, self) or self.wndMain:FindChild("Experience");
+	self.wndCastBar = self:FindRootOrChildWindow("CastBar");
+	self.wndExperience = self:FindRootOrChildWindow("Experience");
 	self.wndHealth = self.wndMain:FindChild("Health:Progress");
 	self.wndShield = self.wndMain:FindChild("Shield");
 	self.wndThreat = self.wndMain:FindChild("Threat");
+	self.wndAuras = self:FindRootOrChildWindow("Auras");
 	-- Temporary Elements
 	self.wndTextLeft = self.wndMain:FindChild("TextLeft");
 	self.wndTextRight = self.wndMain:FindChild("TextRight");
-	-- Auras (HACKY, BOOM)
-	self.wndAuras = self.xmlDoc:LoadForm(self.strLayoutName..self.strUnit.."Auras", nil, self);
-	self.wndBuffs = self.xmlDoc:LoadForm(self.strLayoutName..self.strUnit.."Buffs", nil, self);
-	self.wndDebuffs = self.xmlDoc:LoadForm(self.strLayoutName..self.strUnit.."Debuffs", nil, self);
-	self.wndAuras = self.wndAuras and self.wndAuras:FindChild("Auras") or self.wndMain:FindChild("Auras");
-	self.wndBuffs = self.wndBuffs and self.wndBuffs:FindChild("Buffs") or self.wndMain:FindChild("Buffs");
-	self.wndDebuffs = self.wndDebuffs and self.wndDebuffs:FindChild("Debuffs") or self.wndMain:FindChild("Debuffs");
-	if (self.wndAuras) then self.OnGenerateBuffTooltip = OnGenerateBuffTooltip; self.wndAuras:AddEventHandler("GenerateTooltip", "OnGenerateBuffTooltip", self); end
-	if (self.wndBuffs) then self.OnGenerateBuffTooltip = OnGenerateBuffTooltip; self.wndBuffs:AddEventHandler("GenerateTooltip", "OnGenerateBuffTooltip", self); end
-	if (self.wndDebuffs) then self.OnGenerateBuffTooltip = OnGenerateBuffTooltip; self.wndDebuffs:AddEventHandler("GenerateTooltip", "OnGenerateBuffTooltip", self); end
 
 	-- Expose more Methods
 	self.SetHealth = SetHealth;
@@ -469,7 +438,7 @@ function UnitFrame:New(tUnitFrameController, strLayoutName, strUnit, tXmlData, t
 
 	-- Properties
 	self.tXmlData = tXmlData;
-	self.tAttributes = (tAttributes or {});
+	self.tAttributes = tAttributes or {};
 	self.strUnit = strUnit;
 	self.strLayoutName = strLayoutName;
 	self.bEnabled = false;
@@ -480,6 +449,7 @@ function UnitFrame:New(tUnitFrameController, strLayoutName, strUnit, tXmlData, t
 
 	-- Expose Methods
 	self.LoadForm = LoadForm;
+	self.FindRootOrChildWindow = FindRootOrChildWindow;
 	self.OnMouseEnter = OnMouseEnter;
 	self.OnMouseExit = OnMouseExit;
 	self.OnMouseClick = OnMouseClick;
