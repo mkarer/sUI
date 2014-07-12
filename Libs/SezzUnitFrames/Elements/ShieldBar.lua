bf127013d0eb1ab51cb98998840d1da30304d63a
Martin Karer <sezz@sezz.at>
2014-07-12 23:32:03 +0200

    removed debugging output

--- Libs/SezzUnitFrames/Elements/ShieldBar.lua
+++ Libs/SezzUnitFrames/Elements/ShieldBar.lua
@@ -49,7 +49,7 @@ end
 
 local IsSupported = function(tUnitFrame)
 	local bSupported = (tUnitFrame.wndShield ~= nil);
-	log:debug("Unit %s supports %s: %s", tUnitFrame.strUnit, NAME, string.upper(tostring(bSupported)));
+--	log:debug("Unit %s supports %s: %s", tUnitFrame.strUnit, NAME, string.upper(tostring(bSupported)));
 
 	return bSupported;
 end
