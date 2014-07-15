476dfb91a9b1d511d09cd15bc35d7fa6c2943337
Martin Karer <sezz@sezz.at>
2014-07-16 00:30:56 +0200

    events

--- Core/Temp.lua
+++ Core/Temp.lua
@@ -44,13 +44,13 @@ function M:OnEnable()
 	self:RegisterEvent("Group_MentorLeftAOI", "EventHandler");
 	self:RegisterEvent("Group_MentorRelationship", "EventHandler");
 	self:RegisterEvent("Group_Operation_Result", "EventHandler");
-	self:RegisterEvent("Group_ReadyCheck", "EventHandler");
+--	self:RegisterEvent("Group_ReadyCheck", "EventHandler"); -- initiating_playerindex message
 	self:RegisterEvent("Group_Referral", "EventHandler");
 	self:RegisterEvent("Group_Remove", "EventHandler"); -- someone has been removed, member indexes will change (last frame needs to be disabled)
 	self:RegisterEvent("Group_Request_Result", "EventHandler"); -- nonleader invites someone arg1=player arg2=20/21=? arg3=false=?
 --	self:RegisterEvent("Group_Updated", "EventHandler"); -- happens all the time, no args
 --	self:RegisterEvent("Group_UpdatePosition", "EventHandler"); -- also all the time, table with tables with nIndex to associate unit and coords
-	self:RegisterEvent("UnitNameChanged", "EventHandler");
+--	self:RegisterEvent("UnitNameChanged", "EventHandler"); -- tUnit, strNewName (quest mobs etc)
 end
 
 function S:Test()
