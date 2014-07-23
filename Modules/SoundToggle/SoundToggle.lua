3ec8f2c7e5bd0353b5b05bcc37ca66fa09ecffec
Martin Karer <sezz@sezz.at>
2014-07-23 17:44:11 +0200

    added a fancy status message using FloatText (which is obviously buggy and scales wrong when there are more active messages)

--- Modules/SoundToggle/SoundToggle.lua
+++ Modules/SoundToggle/SoundToggle.lua
@@ -11,6 +11,41 @@ local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
 local M = S:CreateSubmodule("SoundToggle");
 local log, fVolume;
 
+local tTextOption = {
+	strFontFace = "CRB_HeaderGigantic_O",
+	fDuration = 1,
+	fScale = 1,
+	fExpand = 1,
+	fVibrate = 0,
+	fSpinAroundRadius = 0,
+	fFadeInDuration = 0,
+	fFadeOutDuration = 0,
+	fVelocityDirection = 0,
+	fVelocityMagnitude = 0,
+	fAccelDirection = 0,
+	fAccelMagnitude = 0,
+	fEndHoldDuration = 0,
+	eLocation = CombatFloater.CodeEnumFloaterLocation.Top,
+	fOffsetDirection = 0,
+	fOffset = -320,
+	eCollisionMode = CombatFloater.CodeEnumFloaterCollisionMode.Horizontal,
+	fExpandCollisionBoxWidth = 1,
+	fExpandCollisionBoxHeight = 1,
+	nColor = 0xFFD800,
+	iUseDigitSpriteSet = nil,
+	bUseScreenPos = true,
+	bShowOnTop = true,
+	fRotation = 0,
+	fDelay = 0,
+	nDigitSpriteSpacing = 0,
+	arFrames= 	{
+		[1] = { fTime = 0,		fScale = 1.5,	fAlpha = 0.8, },
+		[2] = { fTime = 0.1,	fScale = 1,		fAlpha = 0.8, },
+		[3] = { fTime = 1.1,	fScale = 1,		fAlpha = 0.8,	fVelocityDirection = 0, },
+		[4] = { fTime = 1.3,	fScale = 1,		fAlpha = 0.0,	fVelocityDirection = 0, },
+	},
+};
+
 -----------------------------------------------------------------------------
 -- Initialization
 -----------------------------------------------------------------------------
@@ -45,14 +80,21 @@ end
 function M:ToggleSound()
 	local fCurrentVolume = Apollo.GetConsoleVariable("sound.volumeMaster");
 
+	local tParams = {
+		unitTarget = GameLib.GetControlledUnit(),
+		tTextOption = S:Clone(tTextOption),
+	};
+
 	if (fCurrentVolume == 0) then
-		Print("Sound enabled.");
+		tParams.strText = "Sound enabled.";
 		Apollo.SetConsoleVariable("sound.volumeMaster", self.P.Volume);
 	else
-		Print("Sound disabled.");
+		tParams.strText = "Sound disabled.";
 		self.P.Volume = fCurrentVolume;
 		Apollo.SetConsoleVariable("sound.volumeMaster", 0);
 	end
+
+	Event_FireGenericEvent("Float_RequestShowTextFloater", LuaEnumMessageType.SystemMessage, tParams);
 end
 
 function M:OnSystemKeyDown(nKey)
