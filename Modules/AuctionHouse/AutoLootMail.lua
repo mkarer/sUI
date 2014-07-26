edeafbf49826ca1086fad62254161976052d80fc
Martin Karer <sezz@sezz.at>
2014-07-26 03:46:30 +0200

    maybe fixed the problem that some mails didn't get lootet

--- Modules/AuctionHouse/AutoLootMail.lua
+++ Modules/AuctionHouse/AutoLootMail.lua
@@ -20,10 +20,17 @@ function M:OnInitialize()
 	Flip = Apollo.GetAddon("Flip");
 
 	self:SetEnabledState(S.DB.Modules.AutoLootMail.bEnabled and MagicMail ~= nil);
+
+	Apollo.RegisterTimerHandler("SezzUITimer_AutoLootMail", "CheckMail", self);
+	Apollo.CreateTimer("SezzUITimer_AutoLootMail", 1, false);
 end
 
 function M:OnEnable()
-	self:RegisterEvent("Sezz_NewMailAvailable", "GetMail");
+	self:RegisterEvent("Sezz_NewMailAvailable", "CheckMail");
+	self:RegisterEvent("Sezz_MailAvailable", "CheckMail");
+	self:RegisterEvent("Sezz_NoMailAvailable", "CheckMail");
+	self:RegisterEvent("MailResult", "OnMailResult");
+
 	self:RegisterEvent("LootedItem", "RefreshFlip");
 	self:RegisterEvent("LootedGold", "RefreshFlip");
 end
@@ -37,9 +44,29 @@ function M:GetMail()
 	MagicMail:OnSlashCommand();
 end
 
+function M:CheckMail()
+	local nUnreadMessages, nReadMessages = S:GetMailAmount();
+Print("CheckMail")
+	if (nUnreadMessages + nReadMessages > 0) then
+		self:GetMail();
+
+		if (not self.bStopProcessing) then
+			Apollo.StartTimer("SezzUITimer_AutoLootMail");
+		end
+	end
+end
+
 function M:RefreshFlip()
 	-- Refresh Flip
 	if (Flip and Flip.bLoadOk and Flip.market.wndMain:IsShown()) then
 		Flip.market:RefreshCX();
 	end
+end
+
+function M:OnMailResult(event, eResult)
+   if (eResult == GameLib.CodeEnumGenericError.Mail_MailBoxOutOfRange or eResult == GameLib.CodeEnumGenericError.Item_InventoryFull) then
+   		self.bStopProcessing = true;
+   	else
+   		self.bStopProcessing = false;
+   	end
 end
