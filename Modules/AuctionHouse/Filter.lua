f18629d19f4a2d4c8104251d1033f625a3c271ba
Martin Karer <sezz@sezz.at>
2014-07-27 23:52:01 +0200

    added maximum price filter

--- Modules/AuctionHouse/Filter.lua
+++ Modules/AuctionHouse/Filter.lua
@@ -45,20 +45,20 @@ end
 local tCustomFilter = {
 	-- fnFilter returns true when the auction is filtered and false when it should be displayed.
 	KnownSchematics = {
-		fnFilter = function(self, aucCurrent)
-			local itemCurr = aucCurrent:GetItem();
+		fnFilter = function(self, aucCurr)
+			local itemCurr = aucCurr:GetItem();
 			return (itemCurr:GetActivateSpell() and itemCurr:GetActivateSpell():GetTradeskillRequirements() and itemCurr:GetActivateSpell():GetTradeskillRequirements().bIsKnown);
 		end,
 	},
 	Name = {
-		fnFilter = function(self, aucCurrent)
-			local itemCurr = aucCurrent:GetItem();
+		fnFilter = function(self, aucCurr)
+			local itemCurr = aucCurr:GetItem();
 			return (strfind(strlower(itemCurr:GetName()), self.tFilter.strSearchQueryEscaped) == nil);
 		end,
 	},
 	RuneSlots = {
-		fnFilter = function(self, aucCurrent, nAmount)
-			local itemCurr = aucCurrent:GetItem();
+		fnFilter = function(self, aucCurr, nAmount)
+			local itemCurr = aucCurr:GetItem();
 			local tInfo = itemCurr:GetDetailedInfo();
 			local nRuneSlots = 0;
 			for _, tData in pairs(tInfo) do
@@ -68,6 +68,11 @@ local tCustomFilter = {
 			end
 
 			return (nRuneSlots < nAmount);
+		end,
+	},
+	MaxPrice = {
+		fnFilter = function(self, aucCurr, nAmount)
+			return aucCurr:GetBuyoutPrice():GetAmount() > nAmount;
 		end,
 	},
 };
@@ -122,13 +127,17 @@ function M:GetCurrentFilter()
 			tCustomFilterValues["RuneSlots"] = 0;
 		end
 	end
+	if (self.wndFilters:FindChild("MaxPrice"):IsChecked()) then
+		tinsert(tCustomFilter, "MaxPrice");
+		tCustomFilterValues["MaxPrice"] = self.wndFilters:FindChild("MaxPriceAmount"):GetAmount();
+	end
 
 	return nFamilyId, nCategoryId, nTypeId, strSearchQuery, tFilter, tCustomFilter, tCustomFilterValues;
 end
 
-function M:IsFiltered(aucCurrent)
+function M:IsFiltered(aucCurr)
 	for _, strFilter in ipairs(self.tFilter.tCustomFilter) do
-		if (tCustomFilter[strFilter] and tCustomFilter[strFilter].fnFilter(self, aucCurrent, self.tFilter.tCustomFilterValues[strFilter])) then
+		if (tCustomFilter[strFilter] and tCustomFilter[strFilter].fnFilter(self, aucCurr, self.tFilter.tCustomFilterValues[strFilter])) then
 			return true;
 		end
 	end
