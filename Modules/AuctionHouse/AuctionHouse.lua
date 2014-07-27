2b217b22564d20a8620c89fb0d02199ee4c75066
Martin Karer <sezz@sezz.at>
2014-07-27 11:03:46 +0200

    check guest account, remove events from carbine's auction house

--- Modules/AuctionHouse/AuctionHouse.lua
+++ Modules/AuctionHouse/AuctionHouse.lua
@@ -9,7 +9,8 @@
 
 local S = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:GetAddon("SezzUI");
 local M = S:CreateSubmodule("AuctionHouse", "Gemini:Hook-1.0");
-local log, MarketplaceAuction;
+local log;
+local AccountItemLib = AccountItemLib;
 
 -----------------------------------------------------------------------------
 -- Initialization
@@ -25,18 +26,21 @@ function M:OnInitialize()
 	if (not self.MarketplaceAuction) then
 		self:SetEnabledState(false);
 	end
-end
 
-function M:OnEnable()
-	log:debug("%s enabled.", self:GetName());
-
-	if (not self.MarketplaceAuction._OnToggleAuctionWindow) then
+	if (self.MarketplaceAuction and not self.MarketplaceAuction._OnToggleAuctionWindow) then
 		self.MarketplaceAuction._OnToggleAuctionWindow = self.MarketplaceAuction.OnToggleAuctionWindow;
 		self.MarketplaceAuction.OnToggleAuctionWindow = function(tMarketplaceAuction)
 			tMarketplaceAuction:_OnToggleAuctionWindow();
 			self:Open();
 		end
 	end
+end
+
+function M:OnEnable()
+	log:debug("%s enabled.", self:GetName());
+
+--	self:RegisterEvent("ToggleAuctionWindow", "Open");
+	self:RegisterEvent("AuctionWindowClose", "Close");
 end
 
 function M:OnDisable()
@@ -46,21 +50,45 @@ end
 -----------------------------------------------------------------------------
 
 function M:Open()
+	if (AccountItemLib.CodeEnumEntitlement.EconomyParticipation and AccountItemLib.GetEntitlementCount(AccountItemLib.CodeEnumEntitlement.EconomyParticipation) == 0) then
+		Event_FireGenericEvent("GenericEvent_SystemChannelMessage", Apollo.GetString("CRB_FeatureDisabledForGuests"));
+		return;
+	end
+
 	if (not self.wndMain or not self.wndMain:IsValid()) then
+		self:SetSortOrder("Name", "ASC");
 		self:CreateWindow();
 		self:RegisterEvent("ItemAuctionSearchResults", "OnItemAuctionSearchResults");
 		self.wndMain:Show(true);
-		self:SetSortOrder("Name", "ASC");
+
+		-- Hide Carbine AH
+		self.MarketplaceAuction.wndMain:Show(false, true);
+		Apollo.RemoveEventHandler("ItemAuctionWon", self.MarketplaceAuction);
+		Apollo.RemoveEventHandler("ItemAuctionOutbid", self.MarketplaceAuction);
+		Apollo.RemoveEventHandler("ItemAuctionExpired", self.MarketplaceAuction);
+		Apollo.RemoveEventHandler("ItemAuctionSearchResults", self.MarketplaceAuction);
+		Apollo.RemoveEventHandler("PostItemAuctionResult", self.MarketplaceAuction);
+		Apollo.RemoveEventHandler("ItemAuctionBidResult", self.MarketplaceAuction);
+		Apollo.RemoveEventHandler("ItemCancelResult", self.MarketplaceAuction);
+		Apollo.RemoveEventHandler("UpdateInventory", self.MarketplaceAuction);
+		Apollo.RemoveEventHandler("PlayerCurrencyChanged", self.MarketplaceAuction);
+		Apollo.RemoveEventHandler("OwnedItemAuctions", self.MarketplaceAuction);
 	end
 end
 
 function M:Close()
+	Event_CancelAuctionhouse();
 	self:UnregisterEvent("ItemAuctionSearchResults");
 
 	if (self.wndMain and self.wndMain:IsValid()) then
 		self.wndMain:Destroy();
 	end
 
+	self.wndMain = nil;
+	self.wndSearch = nil;
+	self.wndFilters = nil;
+	self.wndResults = nil;
+	self.wndResultsGrid = nil;
 	self.tFilter = nil;
 	self.tSelectedCategory = nil;
 	self.nSelectedFamily = nil;
@@ -68,7 +96,6 @@ function M:Close()
 	self.bIsSearching = false;
 	self.strSortHeader = nil;
 	self.strSortDirection = nil;
-	self.fnSortResults = nil;
 	self.bFilterChanged = nil;
 	self.tHeaders = nil;
 end
